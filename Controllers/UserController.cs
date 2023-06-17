559979ea5843657a2551a1542e1111d1f4fe11f0
LikDan <likdan.company@gmail.com>
2023-06-18 02:39:14 +0300

    update

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,7 +5,7 @@ using Grpc.Net.Client;
 
 public class UserController {
     public static User User(string access, string refresh, params string[] permissions) {
-        using var channel = GrpcChannel.ForAddress("api:4772");
+        using var channel = GrpcChannel.ForAddress("http://api:4772");
         var client = new Auth.AuthClient(channel);
         var jwt = new JWT {
             Access = access,
